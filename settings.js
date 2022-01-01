50ab5df96a2166f202d18781f735af71ddc3b05a
Kevin Sonney <kevin@sonney.com>
2022-01-01 10:33:11 -0500

    Syntax fix

--- settings.js
+++ settings.js
@@ -207,7 +207,7 @@ var settings = module.exports = {
     //httpAdminRoot: '/admin',
     // Move the admin UI
     //httpAdminRoot: '/editor',
-    httpAdminRoot: false
+    httpAdminRoot: false,
     /** The following property can be used to add a custom middleware function
      * in front of all admin http routes. For example, to set custom http
      * headers. It can be a single function or an array of middleware functions.
