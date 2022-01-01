0b71996f5c880c68c286ef7b87b5b5fc80ece8b5
Kevin Sonney <kevin@sonney.com>
2022-01-01 10:15:31 -0500

    Add crypt key

--- settings.js
+++ settings.js
@@ -79,7 +79,7 @@ var settings = module.exports = {
      */
     //credentialSecret: "a-secret-key",
     // Disabled Credential Secret
-    credentialSecret: false,
+    credentialSecret: "horse-blanket-bingo"
 
     /** By default, the flow JSON will be formatted over multiple lines making
      * it easier to compare changes when using version control.
